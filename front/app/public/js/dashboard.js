0ae202383b0076784c6dd4f40be4921db17ae548
user3 <daniel.miranda.next@gmail.com>
2020-11-25 15:36:18 +0000

    mocks and images for dashboard

--- front/app/public/js/dashboard.js
+++ front/app/public/js/dashboard.js
@@ -1,9 +1,24 @@
  $( document ).ready(function() {
-    var url = "dashboard.json"
+    var mockUser = "user.json";
+    var mockUserMissions = "user-missions.json";
 
-    $.getJSON( url, function(obj) {
-    $.each(obj, function(key, value) {
+   $.getJSON( mockUser, function(obj) {
+        $("")
+
+        $.each(obj, function(key, value) {
             $("#missions").append('<div class="card"><img src="..." class="card-img-top" alt="..."><div class="card-body"><h5 class="card-title">'+value.amount.amount+ ' ' + value.amount.currency +'</h5><p class="card-text">'+value.description+'</p><a href="#" class="btn btn-primary">Me apunto</a></div></div>')
+       });
     });
-    });
+
+    /*$.ajax({
+    url: "http://api.openbeerdatabase.com/v1/beers.json",
+                    data: {
+                                    query: "beer"
+                    },
+                    type: "GET",
+                    success: function(respuesta){
+    console.log("Recibes: ", respuesta);
+    $("<pre>").text(JSON.stringify(respuesta)).appendTo("body");
+                    }
+    });*/
 });
