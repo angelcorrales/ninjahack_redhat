bd55501ddf0fd3d4b16e8e37978d984b15459aad
user2 <federico.garcia-lorca@bbva.com>
2020-11-25 17:44:28 +0000

    recomendadas

--- front/app/public/card/card.js
+++ front/app/public/card/card.js
@@ -1,19 +1,15 @@
  $( document ).ready(function() {
     var mockUser = "dashboard/user.json";
-    var mockUserMissions = "user-missions.json";
 
-   $.getJSON( mockUser, function(response) {
-        $("#avatarImage")[0].src = "/images/" + response.avatar;
-    });
-
-    var urlDashboardMissionUrl = "https://backend-hello-world-aporebote-ninjahack.apps.cluster-142f.142f.example.opentlc.com/hello/greeting";
+    var urlDashboardMissionUrl = "article.json";
 
     $.ajax({
         url: urlDashboardMissionUrl,
         type: "GET",
         success: function(respuesta){
+            console.log(respuesta)
                 $.each(respuesta, function(key, value) {
-                $("#dashboardMissions").append('<div class="card mt-3"><img src=../images/'+value.image+' class="card-img-top" alt="..."><div class="card-body"><h5 class="card-title">'+value.amount.amount+ ' ' + value.amount.currency +'</h5><p class="card-text">'+value.description+'</p><a href="#" class="btn btn-secondary">Reclamar recompensa</a></div></div>')
+                $("#complete").append('<div class="card mt-3"><img src=../images/'+value.image+' class="card-img-top" alt="..."><div class="card-body"><h5 class="card-title">'+value.amount.amount+ ' ' + value.amount.currency +'</h5><p class="card-text">'+value.description+'</p><a href="#" class="btn btn-secondary">Reclamar recompensa</a></div></div>')
             });
         }
     });
