43dbb19870d32185074fe475d881e0b112c99122
user3 <daniel.miranda.next@gmail.com>
2020-11-25 16:42:15 +0000

    change avatar image

--- front/app/public/dashboard/dashboard.js
+++ front/app/public/dashboard/dashboard.js
@@ -3,7 +3,7 @@
     var mockUserMissions = "user-missions.json";
 
    $.getJSON( mockUser, function(response) {
-        $("#avatarImage")[0].src = "/images/" + response.img;
+        $("#avatarImage")[0].src = "/images/" + response.avatar;
     });
 
     var urlDashboardMissionUrl = "https://backend-hello-world-aporebote-ninjahack.apps.cluster-142f.142f.example.opentlc.com/hello/greeting";
@@ -13,7 +13,7 @@
         type: "GET",
         success: function(respuesta){
                 $.each(respuesta, function(key, value) {
-                $("#dashboardMissions").append('<div class="card mt-3"><img src=../images/'+value.image+' class="card-img-top" alt="..."><div class="card-body"><h5 class="card-title">'+value.amount.amount+ ' ' + value.amount.currency +'</h5><p class="card-text">'+value.description+'</p><a href="#" class="btn btn-primary">Me apunto</a></div></div>')
+                $("#dashboardMissions").append('<div class="card mt-3 active"><img src=../images/'+value.image+' class="card-img-top" alt="..."><div class="card-body"><h5 class="card-title">'+value.amount.amount+ ' ' + value.amount.currency +'</h5><p class="card-text">'+value.description+'</p></div></div>')
             });
         }
     });
