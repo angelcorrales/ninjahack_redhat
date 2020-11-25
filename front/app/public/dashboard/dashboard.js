9af2299ec9cbc2bd035545cd573b2b7e27c19913
user2 <federico.garcia-lorca@bbva.com>
2020-11-25 17:09:53 +0000

    recomendadas

--- front/app/public/dashboard/dashboard.js
+++ front/app/public/dashboard/dashboard.js
@@ -13,11 +13,7 @@
         type: "GET",
         success: function(respuesta){
                 $.each(respuesta, function(key, value) {
-<<<<<<< HEAD
                 $("#dashboardMissions").append('<div class="card mt-3"><img src=../images/'+value.image+' class="card-img-top" alt="..."><div class="card-body"><h5 class="card-title">'+value.amount.amount+ ' ' + value.amount.currency +'</h5><p class="card-text">'+value.description+'</p><a href="#" class="btn btn-secondary">Reclamar recompensa</a></div></div>')
-=======
-                $("#dashboardMissions").append('<div class="card mt-3 active"><img src=../images/'+value.image+' class="card-img-top" alt="..."><div class="card-body"><h5 class="card-title">'+value.amount.amount+ ' ' + value.amount.currency +'</h5><p class="card-text">'+value.description+'</p></div></div>')
->>>>>>> 77fee00c779eb8a9e571f8147ef23e4601dbd29b
             });
         }
     });
