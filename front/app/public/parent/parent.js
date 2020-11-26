a6cc22c65fd1f95340bd7a20bf66644e67e0f3c1
user3 <daniel.miranda.next@gmail.com>
2020-11-26 13:55:33 +0000

    add 'recomend' tab for parent

--- front/app/public/parent/parent.js
+++ front/app/public/parent/parent.js
@@ -6,7 +6,7 @@ $( document ).ready(() => {
     });*/
 
     var urlDashboardMissionUrl = "https://backend-users-2-aporebote-ninjahack.apps.cluster-142f.142f.example.opentlc.com/user/missions";
-                                  
+                           
     $.ajax({
         url: urlDashboardMissionUrl,
         type: "GET",
@@ -34,7 +34,7 @@ $( document ).ready(() => {
                 var descripcion = card.find('p').html();
                 var imagen = card.parent().find('img')[0].src;
 
-                $("#reviewedMissions").append('<div class="card mt-3"><img src="'+imagen
+                $("#reviewedMissions").prepend('<div class="card mt-3"><img src="'+imagen
                   +'" class="card-img-top" alt="..."><div class="card-body"><h5 class="card-title">'
                   +cantidad+'</h5><p class="card-text">'
                   +descripcion+'</p><span id="state" class="text-success">Aceptada</span></div></div>');
@@ -49,7 +49,7 @@ $( document ).ready(() => {
                 var descripcion = card.find('p').html();
                 var imagen = card.parent().find('img')[0].src;
 
-                $("#reviewedMissions").append('<div class="card mt-3"><img src="'+imagen
+                $("#reviewedMissions").prepend('<div class="card mt-3"><img src="'+imagen
                   +'" class="card-img-top" alt="..."><div class="card-body"><h5 class="card-title">'
                   +cantidad+'</h5><p class="card-text">'
                   +descripcion+'</p><span id="state" class="text-danger">Rechazada</span></div></div>');
@@ -62,6 +62,29 @@ $( document ).ready(() => {
                     $('#emptyImage')[0].hidden = false
                 }
             }
+        }
+    });
+        
+    var urlMissions = "https://backend-missions-aporebote-ninjahack.apps.cluster-142f.142f.example.opentlc.com/missions";
+          
+    $.ajax({
+        url: urlMissions,
+        type: "GET",
+        success: function(respuesta){
+            $.each(respuesta, function(key, value) {
+                $("#recomend").append('<div class="card mt-3"><img src=../images/'
+                    +value.image+' class="card-img-top" alt="..."><div class="card-body"><h5 class="card-title">'
+                    +value.amount.amount+ ' ' + value.amount.currency 
+                    +'</h5><p class="card-text">'+value.description
+                    +'</p><div class="row justify-content-between m-0"><button class="btn btn-primary recomend-btn">Recomendar</button></div></div></div>');
+            });
+
+            $('.recomend-btn').click(function() {
+                $(this).addClass('btn-success');
+                $(this).removeClass('btn-primary');
+                $(this).html('Recomendada');
+                $(this).disable = true;
+            })
         }
     });
 });
