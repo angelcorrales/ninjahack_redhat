22849d93892073080b6ed9518805afbb8761abc1
user2 <federico.garcia-lorca@bbva.com>
2020-11-26 09:27:28 +0000

    apuntado

--- front/app/public/js/utils.js
+++ front/app/public/js/utils.js
@@ -6,7 +6,7 @@ $.ajax({
     type: "GET",
     success: function(respuesta){
         $.each(respuesta, function(key, value) {
-            $("#missions").append('<div class="card mt-3"><img src=../images/'+value.image+' class="card-img-top" alt="..."><div class="card-body"><h5 class="card-title">'+value.amount.amount+ ' ' + value.amount.currency +'</h5><p class="card-text">'+value.description+'</p><a href="#" class="btn btn-primary mission">Me apunto</a></div></div>')
+            $("#missions").append('<div class="card mt-3"><img src=../images/'+value.image+' class="card-img-top" alt="..."><div class="card-body"><h5 class="card-title">'+value.amount.amount+ ' ' + value.amount.currency +'</h5><span class="badge badge-info">Misión disponible</span><p class="card-text">'+value.description+'</p><a href="#" class="btn btn-primary mission">Me apunto</a></div></div>')
         });
         $( ".mission" ).click(function() {
             $(this).removeClass('btn-primary').addClass('btn-success').html('Apuntado').addClass('disabled')
